e55622fd234dae376cfc6f339367b6e3a3043142
blauury <lazaltron@fecea.edu.ar>
2026-06-16 10:52:01 -0300

    fixes de typos

--- src/schemas/alumno.schema.js
+++ src/schemas/alumno.schema.js
@@ -2,21 +2,21 @@ const joi = require('joi');
 
 const id = joi.number().integer().positive().min(1);
 const nombre = joi.string().min(3).max(50).required();
-const email = joi.string().email().required();
+const mail = joi.string().email().required();
 const usuario = joi.string().min(3).max(30).required();
 const password = joi.string().min(6).max(60).required();
 
 
 const createAlumnoSchema = joi.object({
     nombre: nombre.required(),
-    email: email.required(),
+    mail: mail.required(),
     usuario: usuario.required(),
     password: password.required(),
 });
 
 const updateAlumnoSchema = joi.object({
     nombre: nombre,
-    email: email,
+    mail: mail,
     usuario: usuario,
     password: password,
 });
